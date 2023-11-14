dd6ce9f8c9fe7e066d69411aa96f88ade9b0616d
Dhruv Joshi <jdhruv1503@gmail.com>
2023-11-14 11:46:48 +0530

    fix

--- app/app.py
+++ app/app.py
@@ -95,7 +95,7 @@ def run_model(model, context, query):
         print(type(resp))
         return resp
     elif model in AVAIL_PT:
-        pipe = pipeline("conversational", model=model, device=0, model_kwargs={'from_pt': True})
+        pipe = pipeline("conversational", model=model, device=0, kwargs={'from_pt': True})
         resp = pipe([Conversation(f"You are a helpful chatbot. Here's a series of excerpts from Gandhi's memoirs, some in 3rd and some in 1st person:\n\n\n\n {context} \n\n\n\n Now, using this information, correctly and concisely answer the following question: {query}")])
         print(type(resp))
         return resp
@@ -118,4 +118,4 @@ def init_models():
         elif model in AVAIL_CHATBOTS:
             pipe.append(pipeline("conversational", model=model, device=0))
         elif model in AVAIL_PT:
-            pipe.append(pipeline("conversational", model=model, device=0, model_kwargs={'from_pt': True}))
+            pipe.append(pipeline("conversational", model=model, device=0, kwargs={'from_pt': True}))
