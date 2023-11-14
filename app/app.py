5b63615a839808b19e52b37821e32a177dd6ed75
Dhruv Joshi <jdhruv1503@gmail.com>
2023-11-14 12:10:25 +0530

    fix

--- app/app.py
+++ app/app.py
@@ -11,19 +11,12 @@ AVAIL_MODELS = ["deepset/roberta-base-squad2",
                 "deepset/tinyroberta-squad2",
                 "deepset/minilm-uncased-squad2"]
 
-AVAIL_MODELS = []
-
 AVAIL_CHATBOTS = [
                   "microsoft/DialoGPT-small",
                   "microsoft/DialoGPT-medium",
                   "microsoft/DialoGPT-large",
                   
                   ]
-
-AVAIL_CHATBOTS = []
-
-AVAIL_PT = [
-                  "allenai/cosmo-xl"]
 
 app = FastAPI(title="COL106 A7 custom API lesssgooooooo")
 origins = ["*"]
@@ -87,11 +80,6 @@ def run_model(model, context, query):
         resp = pipe([Conversation(f"You are a helpful chatbot. Here's a series of excerpts from Gandhi's memoirs, some in 3rd and some in 1st person:\n\n\n\n {context} \n\n\n\n Now, using this information, correctly and concisely answer the following question: {query}")])
         print(type(resp))
         return resp
-    elif model in AVAIL_PT:
-        pipe = pipeline("conversational", model=model, device=0, kwargs={'from_pt': True})
-        resp = pipe([Conversation(f"You are a helpful chatbot. Here's a series of excerpts from Gandhi's memoirs, some in 3rd and some in 1st person:\n\n\n\n {context} \n\n\n\n Now, using this information, correctly and concisely answer the following question: {query}")])
-        print(type(resp))
-        return resp
     else:
         return {'error': 'model not found'}
 
@@ -102,19 +90,9 @@ def init_models():
     lis = AVAIL_CHATBOTS
     for k in AVAIL_MODELS:
         lis.append(k)
-    for k in AVAIL_PT:
-        lis.append(k)
 
     for model in lis:
         if model in AVAIL_MODELS:
             pipe.append(pipeline("question-answering", model=model, device=0))
         elif model in AVAIL_CHATBOTS:
             pipe.append(pipeline("conversational", model=model, device=0))
-        elif model in AVAIL_PT:
-            if model.startswith("facebook") or model.startswith("microsoft") or model.startswith("allenai"):
-                print("here")
-                tokenizer = AutoTokenizer.from_pretrained(model)
-                model2 = AutoModelForSeq2SeqLM.from_pretrained("allenai/cosmo-xl").to('cuda')
-            elif model.startswith("Pygmalion"):
-                tokenizer = AutoTokenizer.from_pretrained(model)
-                model2 = AutoModelForCausalLM.from_pretrained(model, kwargs={'from_pt':True})
